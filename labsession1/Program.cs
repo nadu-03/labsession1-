85fd1630e42b9a76c193442cfab79bbb075ccf42
nadu <nadu@WINDOWS-UINETAB>
2024-01-29 11:06:39 -0800

    Add project files.

---
+++ labsession1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsession1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the length of the rectangle:");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("enter the width of the rectangle:");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = (length * width);
+            Console.WriteLine("th area of rectangle is:" + area);
+            Console.ReadLine();
+        }
+    }
+}
