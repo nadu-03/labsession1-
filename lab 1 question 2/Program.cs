0bce32a81f828d6d53dde37702e2124aa1861c58
nadu <nadu@WINDOWS-UINETAB>
2024-01-29 20:48:08 -0800

    labsheet 01 question2

---
+++ lab 1 question 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number; 
+            for (int i=0; i<10; i++)
+            {
+                Console.WriteLine("\nEnter a number:");
+            }
+            
+           
+             Console.WriteLine("enter a number:");
+             number= int.Parse(Console.ReadLine());
+             if (number %2== 0)
+             {
+               Console.WriteLine("it's Even ");
+                    
+             }
+                
+            else
+            {
+                    Console.WriteLine( "is odd");
+                 
+            }
+            
+        }
+    }
+}
